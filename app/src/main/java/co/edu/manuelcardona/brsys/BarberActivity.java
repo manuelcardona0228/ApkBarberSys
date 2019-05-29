d012338fccec06c71cf7567df8f0c9972f5b99ac
Manuel <manuel.cardonam@autonoma.edu.co>
2019-05-28 22:20:52 -0500

    Se agregaron nuevas activities

---
+++ app/src/main/java/co/edu/manuelcardona/brsys/BarberActivity.java
@@ -0,0 +1,13 @@
+package co.edu.manuelcardona.brsys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BarberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barber);
+    }
+}
--- app/src/main/java/co/edu/manuelcardona/brsys/LoginActivity.java
+++ app/src/main/java/co/edu/manuelcardona/brsys/LoginActivity.java
@@ -1,5 +1,6 @@
 package co.edu.manuelcardona.brsys;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,6 +25,11 @@ public class LoginActivity extends AppCompatActivity {
 
     private RequestQueue mRequestQueue;
     private String baseUrl = "http://192.168.0.18:8000/api/";
+    private static final String EXTRA_NAME = "co.edu.manuelcardona.brsys.NAME";
+    private static final String EXTRA_LASTNAME = "co.edu.manuelcardona.brsys.LASTNAME";
+    private static final String EXTRA_PHONE = "co.edu.manuelcardona.brsys.PHONE";
+    private static final String EXTRA_ID = "co.edu.manuelcardona.brsys.ID";
+    private static final String EXTRA_TYPEUSER = "co.edu.manuelcardona.brsys.TYPEUSER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +42,13 @@ public class LoginActivity extends AppCompatActivity {
     public void onLogin(View view)
     {
         String localUrl = baseUrl + "login";
+
         String name = "";
+        String lastName = "";
+        String phone = "";
+        int id = 0;
+        int type_user = 0;
+
         String email = ((EditText) findViewById(R.id.tEmail)).getText().toString();
         String password = ((EditText) findViewById(R.id.tPassword)).getText().toString();
 
@@ -51,19 +63,50 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response)
                     {
+                        String name = "desconocido";
+                        String lastName = "desconocido";
                         String phone = "desconocido";
-
+                        int id = 0;
+                        int type_user = 0;
                         try {
                             JSONObject datos = response.getJSONObject("data");
+                            name = datos.getString("name");
+                            lastName = datos.getString("lastName");
                             phone = datos.getString("phone");
+                            id = datos.getInt("id");
+                            type_user = datos.getInt("type_user_id");
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
 
-                        String mensaje = "El usuario " + phone + " inicio correctamente.";
+                        String mensaje = "El usuario " + name + " " + lastName + " "+ type_user +" inicio correctamente.";
 
                         Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_LONG).show();
                         Log.i("post", "Logeado con éxito");
+
+                        if(type_user == 3)
+                        {
+                            Intent intent = new Intent(getApplicationContext(), BarberActivity.class);
+                            intent.putExtra(EXTRA_NAME, name);
+                            intent.putExtra(EXTRA_LASTNAME, lastName);
+                            intent.putExtra(EXTRA_PHONE, phone);
+                            intent.putExtra(EXTRA_ID, id);
+                            intent.putExtra(EXTRA_TYPEUSER, type_user);
+                            startActivity(intent);
+                        }
+
+                        if(type_user == 4)
+                        {
+                            Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+                            intent.putExtra(EXTRA_NAME, name);
+                            intent.putExtra(EXTRA_LASTNAME, lastName);
+                            intent.putExtra(EXTRA_PHONE, phone);
+                            intent.putExtra(EXTRA_ID, id);
+                            intent.putExtra(EXTRA_TYPEUSER, type_user);
+                            startActivity(intent);
+                        }
+
+
                     }
                 },
                 new Response.ErrorListener() {
